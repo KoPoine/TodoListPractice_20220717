7394c2fa78f1d1220f9f43e257be89d2503105ec
김민철 <kmc901206@gmail.com>
2022-07-17 12:52:00 +0900

    ListView 작성 완료

--- app/src/main/java/com/neppplus/todolistpractice_20220717/adapters/TodoListAdapter.kt
+++ app/src/main/java/com/neppplus/todolistpractice_20220717/adapters/TodoListAdapter.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
+import com.neppplus.todolistpractice_20220717.R
 import com.neppplus.todolistpractice_20220717.datas.TodoData
+import com.willy.ratingbar.ScaleRatingBar
 
 class TodoListAdapter (
     val mContext : Context,
@@ -21,6 +24,13 @@ class TodoListAdapter (
 
         val row = tempRow!!
 
+        val data = mList[position]
+
+        val ratingBar = row.findViewById<ScaleRatingBar>(R.id.todoRatingBar)
+        val todoTitle = row.findViewById<TextView>(R.id.todoTitleTxt)
+
+        ratingBar.rating = data.rating.toFloat()
+        todoTitle.text = data.todoTitle
 
         return row
     }
