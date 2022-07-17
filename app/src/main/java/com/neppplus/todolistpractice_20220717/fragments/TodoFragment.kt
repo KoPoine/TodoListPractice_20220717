6b52cbd8c2a472d3ca67412c26d85383be169369
김민철 <kmc901206@gmail.com>
2022-07-17 12:42:26 +0900

    TodoData라는 데이터 클래스 생성
    더미데이터 생성

--- app/src/main/java/com/neppplus/todolistpractice_20220717/fragments/TodoFragment.kt
+++ app/src/main/java/com/neppplus/todolistpractice_20220717/fragments/TodoFragment.kt
@@ -6,8 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.neppplus.todolistpractice_20220717.R
+import com.neppplus.todolistpractice_20220717.datas.TodoData
 
 class TodoFragment : Fragment() {
+
+    val todoList = ArrayList<TodoData>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,6 +18,17 @@ class TodoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_todo, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        todoList.add( TodoData(4.5, "오늘 수업 수강하기", "neppPlus 학원", false) )
+        todoList.add( TodoData(4.0, "오늘 수업 복습하기", "집", false) )
+        todoList.add( TodoData(3.5, "오늘 수업 출석하기", "neppPlus 학원", true) )
+        todoList.add( TodoData(3.0, "오늘 수업 수강하기", "neppPlus 학원", false) )
+        todoList.add( TodoData(2.5, "오늘 수업 수강하기", "neppPlus 학원", false) )
+
     }
 
 }
