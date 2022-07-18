2282ef2c071a97f55578d2908b89401bd60d912a
KoPoine <kmc901206@gmail.com>
2022-07-18 19:46:19 +0900

    BottomNav + ViewPager2 3. ViewPager2 어댑터 제작 완료

--- app/src/main/java/com/neppplus/todolistpractice_20220717/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/neppplus/todolistpractice_20220717/adapters/MainViewPagerAdapter.kt
@@ -1,18 +1,20 @@
 package com.neppplus.todolistpractice_20220717.adapters
 
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.neppplus.todolistpractice_20220717.fragments.GoalFragment
 import com.neppplus.todolistpractice_20220717.fragments.SettingFragment
 import com.neppplus.todolistpractice_20220717.fragments.TodoFragment
 
-class MainViewPagerAdapter (fm : FragmentManager) : FragmentPagerAdapter(fm) {
-    override fun getCount(): Int {
+class MainViewPagerAdapter (fa : FragmentActivity) : FragmentStateAdapter(fa) {
+//    몇장짜리 ViewPager인지
+    override fun getItemCount(): Int {
         return 3
     }
 
-    override fun getItem(position: Int): Fragment {
+//    각 포지션에 어떤 프래그먼트를 보낼껀지
+    override fun createFragment(position: Int): Fragment {
         return when (position) {
             0 -> TodoFragment()
             1 -> GoalFragment()
@@ -20,11 +22,4 @@ class MainViewPagerAdapter (fm : FragmentManager) : FragmentPagerAdapter(fm) {
         }
     }
 
-    override fun getPageTitle(position: Int): CharSequence? {
-        return when (position) {
-            0 -> "TodoList"
-            1 -> "Goal"
-            else -> "Setting"
-        }
-    }
 }
