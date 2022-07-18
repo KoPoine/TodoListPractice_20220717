a7046f57ad286e94c3b828f43e2d90573d879800
KoPoine <kmc901206@gmail.com>
2022-07-18 19:55:30 +0900

    BottomNav + ViewPager2 4. ViewPager2, BottomNav 연동까지 완료

--- app/src/main/java/com/neppplus/todolistpractice_20220717/MainActivity.kt
+++ app/src/main/java/com/neppplus/todolistpractice_20220717/MainActivity.kt
@@ -2,6 +2,7 @@ package com.neppplus.todolistpractice_20220717
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
 import com.neppplus.todolistpractice_20220717.adapters.MainViewPagerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,9 +14,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mPagerAdapter = MainViewPagerAdapter(supportFragmentManager)
+        mPagerAdapter = MainViewPagerAdapter(this)
         mainViewPager.adapter = mPagerAdapter
 
-        mainTabLayout.setupWithViewPager(mainViewPager)
+//        바텀 네비게이션 클릭 이벤트 처리
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> mainViewPager.currentItem = 0
+                R.id.paper -> mainViewPager.currentItem = 1
+                R.id.profile -> mainViewPager.currentItem = 2
+            }
+            return@setOnItemSelectedListener true
+        }
+
+//        뷰 페이저 연결
+        mainViewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                bottomNav.menu.getItem(position).isChecked = true
+            }
+        })
     }
 }
