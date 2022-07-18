ae56a574e0e26c3df79f29413f0156ce958ae7c1
KoPoine <kmc901206@gmail.com>
2022-07-18 19:32:51 +0900

    RecyclerView 제작 완료 5. 실제 데이터 모두 연결 완료

--- app/src/main/java/com/neppplus/todolistpractice_20220717/adapters/TodoRecyclerViewAdapter.kt
+++ app/src/main/java/com/neppplus/todolistpractice_20220717/adapters/TodoRecyclerViewAdapter.kt
@@ -1,12 +1,17 @@
 package com.neppplus.todolistpractice_20220717.adapters
 
 import android.content.Context
+import android.graphics.Paint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.neppplus.todolistpractice_20220717.R
 import com.neppplus.todolistpractice_20220717.datas.TodoData
+import com.willy.ratingbar.ScaleRatingBar
+import kotlinx.android.synthetic.main.todo_list_item.view.*
 
 class TodoRecyclerViewAdapter(
     val mContext : Context,
@@ -16,7 +21,33 @@ class TodoRecyclerViewAdapter(
     inner class MyViewHolder(view : View) : RecyclerView.ViewHolder(view) {
         fun bind(item : TodoData) {
 //            실제적으로 한칸의 xml(itemView > MyViewHolder의 생성자로 만든 view)과 한칸의 데이터(item: TodoData)를 연결
+            val todoRatingBar = itemView.findViewById<ScaleRatingBar>(R.id.todoRatingBar)
+            val todoTitleTxt = itemView.findViewById<TextView>(R.id.todoTitleTxt)
+            val todoCb = itemView.findViewById<CheckBox>(R.id.todoCb)
 
+            todoRatingBar.rating = item.rating.toFloat()
+            todoTitleTxt.text = item.todoTitle
+            todoCb.isChecked = item.isFinished
+
+//            1) 체크 박스가 체크되어있다면 > 완료되었기에 TextView에 취소선 긋기
+            if (item.isFinished) {
+                todoTitleTxt.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
+            }
+            else {
+                todoTitleTxt.paintFlags = 0
+            }
+
+//            2) 체크 박스가 클릭되었을때 (체크 박스 여부 변경 감지 이벤트) > TextView에 취소선 긋기
+            todoCb.setOnCheckedChangeListener { compoundButton, isChecked ->
+                if (isChecked) {
+//                    체크 박스가 체크 되었을때의 로직
+                    todoTitleTxt.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
+                }
+                else {
+//                    체크 박스가 체크 해제되었을때의 로직
+                    todoTitleTxt.paintFlags = 0
+                }
+            }
         }
     }
 
